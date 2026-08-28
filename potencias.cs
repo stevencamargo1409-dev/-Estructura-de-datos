b653067abd53eccd1d89427c56b49361dd90702a
stevencamargo1409-dev <stevencamargo1409@gmail.com>
2026-08-28 13:31:52 -0500

    Implement sum and power display methods
    Added methods to print sum and calculate power.

---
+++ potencias.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+        //procedimiento suma e imprimir
+        public static void ImprimirSuma(int a, int b)
+        {
+
+
+  
+            int suma = a + b;
+            Console.WriteLine(suma);
+
+        }
+         
+
+
+
+         public static void MostrarPotencia(double num1,double pot )
+         {
+           double pote = Math.Pow(num1 , pot);
+            Console.WriteLine($"la potencia de {num1} elevado a {pot} es:{pote}");
+
+
+         }
+        
+
+        static void Main(string[] args)
+        {
+            int x = 12 , y = 23 ;
+
+            ImprimirSuma  (y,x);         //x,y             //(argumentos actuales) 
+            ImprimirSuma  (1000, -1);   //1000,-1         //(argumentos literales)
+
+            double num1 = 12, pot = 3;
+            MostrarPotencia (num1, pot);
+        }
