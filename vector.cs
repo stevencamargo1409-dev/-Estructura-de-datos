82e681113831eac2e0fef87e09baf0743119dcf9
stevencamargo1409-dev <stevencamargo1409@gmail.com>
2026-09-18 13:04:53 -0500

    Add vector.cs with basic array example

---
+++ vector.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[5]
+                { 15, 42, 8, -4, 99 };
+            Console.WriteLine($"el elemento en [0]: {numeros[0]}");
+        }
+    }
+}
